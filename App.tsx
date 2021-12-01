5f248556269f80f6786355ade587356992928c25
Vitalii Danylov <vitaliidanylov1992@gmail.com>
2021-12-01 20:20:42 +0100

    Add list and listItem

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import {
   ActivityIndicator,
   Button,
+  FlatList,
+  Image,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
+  useWindowDimensions,
   View,
 } from 'react-native'
 import { loadHotels } from './src/api'
 
 type StatusType = 'success' | 'error' | 'loading'
+const DEFAULT_OFFSET = 24
 
 export default function App() {
   const [hotels, setHotels] = useState()
   const [status, setStatus] = useState<StatusType>('loading')
+  const { width } = useWindowDimensions()
 
   const setStatusCallback = (status: StatusType) => () => setStatus(status)
 
@@ -35,6 +41,35 @@ export default function App() {
     })
   }
 
+  const ItemSeparatorComponent = useCallback(
+    () => <View style={{ height: DEFAULT_OFFSET }} />,
+    []
+  )
+
+  const renderHotelItem = ({ item: hotel }: { item: any }) => {
+    const uri = hotel?.gallery?.[0]
+    return (
+      <View
+        key={hotel.id}
+        style={[styles.itemContainer, styles.itemContainerShadow]}
+      >
+        {uri ? (
+          <Image
+            source={{ uri }}
+            style={styles.image}
+            defaultSource={require('./assets/no-preview.png')}
+          />
+        ) : (
+          <View style={styles.image} />
+        )}
+        <View style={{ padding: DEFAULT_OFFSET / 2 }}>
+          <Text>{hotel.name}</Text>
+          <Text>{hotel.name}</Text>
+        </View>
+      </View>
+    )
+  }
+
   if (status === 'error') {
     return (
       <View style={styles.errorContainer}>
@@ -47,11 +82,13 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      {status === 'loading' ? (
-        <ActivityIndicator size="large" />
-      ) : (
-        <Text>{JSON.stringify(hotels, null, 2)}</Text>
-      )}
+      <FlatList
+        data={hotels}
+        refreshing={status === 'loading'}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        renderItem={renderHotelItem}
+        contentContainerStyle={styles.scrollViewContent}
+      />
     </SafeAreaView>
   )
 }
@@ -59,9 +96,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  errorContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  errorContainer: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    backgroundColor: '#ffffff',
+  },
+  itemContainerShadow: {
+    shadowColor: '#000000',
+    shadowRadius: 12,
+    shadowOpacity: 0.2,
+    elevation: 6,
+  },
+  scrollViewContent: {
+    padding: DEFAULT_OFFSET,
+  },
 })
